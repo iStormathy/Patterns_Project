91f419fdb0f97a21a001a7afce371c061abb4485
iStormathy <stormageddon473@yahoo.com>
2026-04-17 07:58:14 -0400

    Added notes

--- Patterns_Project/Patterns_Project.cpp
+++ Patterns_Project/Patterns_Project.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main()
 {
     int row = 10;
+    // First pattern increments until there are 10 rows
     for (int a = 1; a <= row; a++)
     {
         for (int b = 1; b <= a; b++)
@@ -12,6 +13,7 @@ int main()
         }
         cout << endl;
     }
+    // Second pattern decrements until the rows are back to 1
     for (int a = row - 1; a >= 1; a--)
     {
         for (int b = 1; b <= a; b++)
